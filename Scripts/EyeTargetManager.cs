d86740cda48e6f13d97360146cef493cca471ce4
Ryan Evans <me@rjevans.net>
2018-10-22 22:58:35 -0700

    First pass of confidence based meshing

--- Scripts/EyeTargetManager.cs
+++ Scripts/EyeTargetManager.cs
@@ -8,14 +8,39 @@ namespace RoomMeshing
 {
     public class EyeTargetManager : MonoBehaviour
     {
+        public enum GeneratorType
+        {
+            Spherical,
+            ConfidenceBased
+        }
+
         public Locator MainLocator;
         public string MainScene;
+        public GeneratorType TypeToUse;
 
         public TargetGenerator SphericalGenerator;
+        public TargetGenerator ConfidenceBasedGenerator;
+
+        public TargetGenerator ActiveGenerator
+        {
+            get
+            {
+                switch(TypeToUse)
+                {
+                    case GeneratorType.Spherical:
+                        return SphericalGenerator;
+                    default:
+                    case GeneratorType.ConfidenceBased:
+                        return ConfidenceBasedGenerator;
+                }
+            }
+        }
 
         // Use this for initialization
         void Start()
         {
+            var mapper = FindObjectOfType<MLSpatialMapper>();
+            mapper.RefreshAllMeshes();
             if (!MLEyes.IsStarted)
             {
                 MLEyes.Start();
@@ -25,14 +50,22 @@ namespace RoomMeshing
                 MLInput.Start();
             }
             MLInput.OnControllerButtonDown += OnButtonDown;
-            SphericalGenerator.InitializeGenerator(MainLocator);
+            var gen = ActiveGenerator;
+            DisableOtherGenerators(gen);
+            gen.InitializeGenerator(MainLocator);
+        }
+
+        void DisableOtherGenerators(TargetGenerator active)
+        {
+            SphericalGenerator.enabled = SphericalGenerator == active;
+            ConfidenceBasedGenerator.enabled = ConfidenceBasedGenerator == active;
         }
 
         private void OnButtonDown(byte arg1, MLInputControllerButton button)
         {
             if(button == MLInputControllerButton.Bumper)
             {
-                SphericalGenerator.Abort();
+                ActiveGenerator.Abort();
             }
         }
 
@@ -55,7 +88,7 @@ namespace RoomMeshing
 
         void Update()
         {
-            if(SphericalGenerator.IsDone())
+            if(ActiveGenerator.IsDone())
             {
                 ProceedToMainScene();
                 return;
